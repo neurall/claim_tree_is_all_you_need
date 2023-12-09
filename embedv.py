cf7e8c0f7fc3f96245a0be6e6133875bbb40461f
neurall <neuralll@gmail.com>
2023-12-09 22:10:12 +1100

    new source comments

--- embedv.py
+++ embedv.py
@@ -1,30 +1,56 @@
+# Author: Ladislav Nevery
+# Date: 1.12.2023
+# Copyright (c) 2023 Ladislav Nevery. All rights reserved.
+
+# Importing necessary libraries
 from sentence_transformers import SentenceTransformer
 import pandas as pd
-import os,chromadb,time
-from clean import *
-dirname='verified-claims';total=0
-csv_dir='../data_dry/politifact/'+dirname+'/'
+import os, chromadb, time
+from clean import *  # Assuming this is a custom module for text cleaning
+
+# Setup for directory and initial variables
+total = 0
+dirname = 'verified-claims'
+csv_dir = '../data_dry/politifact/' + dirname + '/'
+
+# Loading the Sentence Transformer model
 model = SentenceTransformer('WhereIsAI/UAE-Large-V1', device="cpu")
-client = chromadb.PersistentClient(path=dirname+".db")
-collection = client.get_or_create_collection(name='data', metadata={"hnsw:space": "l2"}) # or ip or cosine
+
+# Setting up a database client and creating or retrieving a data collection
+client = chromadb.PersistentClient(path=dirname + ".db")
+collection = client.get_or_create_collection(name='data', metadata={"hnsw:space": "l2"})
+
+# Processing each CSV file in the designated directory
 for file in os.listdir(csv_dir):
     if file.endswith('.csv'):
-        fn=file.split('.')[0]
+        fn = file.split('.')[0]
         print(file)
-        df = pd.read_csv(csv_dir+'/'+file, header=None, names=['blk','claim','date','verified_claim_url','true','speaker','verified_claim','article'])
-        df['ids'] = df.reset_index().apply(lambda row: file + ',' +str(row['index']) , axis=1).tolist()# df['article'] = df['article'].fillna('') # df['docs'] = df.apply(lambda row: clean_text(row['article']), axis=1).tolist()
+
+        # Reading CSV file with specified column names
+        df = pd.read_csv(csv_dir + '/' + file, header=None, names=['blk', 'claim', 'date', 'verified_claim_url', 'true', 'speaker', 'verified_claim', 'article'])
+
+        # Generating unique identifiers for each row
+        df['ids'] = df.reset_index().apply(lambda row: file + ',' + str(row['index']), axis=1).tolist()
+        
+        # Handling missing speaker values and creating a cleaned document column
         df['speaker'] = df['speaker'].fillna('')
-        df['docs'] = df.apply(lambda row: row['speaker'].lower()+' says '+clean_text(row['verified_claim'].replace('says','').strip())+' at '+row['date'], axis=1).tolist()
-        df['urls'] = df.reset_index().apply(lambda row: row['verified_claim_url'].split('statements/')[1].rstrip('/') , axis=1).tolist()
-        total=0;cnt=100;start=time.time()
+        df['docs'] = df.apply(lambda row: row['speaker'].lower() + ' says ' + clean_text(row['verified_claim'].replace('says', '').strip()) + ' at ' + row['date'], axis=1).tolist()
+
+        # Extracting URLs from the verified_claim_url field
+        df['urls'] = df.reset_index().apply(lambda row: row['verified_claim_url'].split('statements/')[1].rstrip('/'), axis=1).tolist()
+
+        # Initializing variables for performance measurement
+        total = 0
+        cnt = 100
+        start = time.time()
+
+        # Processing and encoding the data in batches
         for i in range(0, df.shape[0], cnt):
-            cf = df[i:i+cnt]
+            cf = df[i:i + cnt]
             embeddings = model.encode(cf['docs'].tolist()).tolist()
             collection.upsert(ids=[str(id) for id in cf['ids'].tolist()], embeddings=embeddings, metadatas=[{'url': url} for url in cf['urls'].tolist()])
-            sofar=time.time()-start
-            total+=cnt
-            print(sofar,sofar/total,total)
-
-
-
-
+            
+            # Performance tracking
+            sofar = time.time() - start
+            total += cnt
+            print(sofar, sofar / total, total)
--- summarize.py
+++ summarize.py
@@ -1,43 +1,61 @@
-import pandas as pd
-import os,chromadb,openai
-from datetime import datetime
-from clean import *
+# Author: Ladislav Nevery
+# Date: 1.12.2023
+# Copyright (c) 2023 Ladislav Nevery. All rights reserved.
 
-dirname='fact-checking';total=0
-csv_dir='../data_dry/politifact/'+dirname+'/'
+# Importing necessary libraries
+import pandas as pd
+import os, chromadb, openai
+from datetime import datetime
+from clean import *  # Assuming this is a custom module for text cleaning
+
+dirname = 'fact-checking'; total = 0
+csv_dir = '../data_dry/politifact/' + dirname + '/'
+
+# Iterating over each file in the specified directory
 for file in os.listdir(csv_dir):
     if file.endswith('.csv'):
-        fn=file.split('.')[0]        
+        fn = file.split('.')[0]        
         print(file)
-        dt=0
-        df = pd.read_csv(csv_dir+'/'+file)
+        dt = 0
+
+        # Reading the main CSV file
+        df = pd.read_csv(csv_dir + '/' + file)
+
+        # Trying to read a transcript file; if not found, skip to the next file
         try:
-            dt = pd.read_csv(csv_dir.replace(dirname,'transcripts')+'/'+file.replace('.csv','.tsv'),sep='\t', index_col=False, header=None, names=['id','speaker','text','no'])
+            dt = pd.read_csv(csv_dir.replace(dirname, 'transcripts') + '/' + file.replace('.csv', '.tsv'), sep='\t', index_col=False, header=None, names=['id', 'speaker', 'text', 'no'])
         except:
-            print('error',file)
+            print('error', file)
             continue
-        sb=dt.drop(['id','no'],axis=1)
+
+        # Preparing the data for summary generation
+        sb = dt.drop(['id', 'no'], axis=1)
         of = df[['line_number']].copy()
         df = df.assign(summary='')
         df_list = sb.values.tolist()
         debate = [' '.join(map(str, row)) for row in df_list]
-        date = datetime.strptime(fn.split('_',1)[0], '%Y%m%d')
+
+        # Formatting the date
+        date = datetime.strptime(fn.split('_', 1)[0], '%Y%m%d')
         when = date.strftime('%B %d, %Y')
         day = int(date.strftime('%d'))
-        if 4 <= day <= 20 or 24 <= day <= 30:
-            suffix = "th"
-        else:
-            suffix = ["st", "nd", "rd"][day % 10 - 1]
+        suffix = "th" if 4 <= day <= 20 or 24 <= day <= 30 else ["st", "nd", "rd"][day % 10 - 1]
         when = when.replace(date.strftime('%d'), str(day) + suffix)
-        where=fn.split('_',1)[1].replace('_',' ')
-        txt=debate
+
+        where = fn.split('_', 1)[1].replace('_', ' ')
+        txt = debate
+
+        # Iterating over rows in the dataframe
         for index, row in df.iterrows():
-            to=int(row['line_number'])
-            frm=max(0,(to-1)-10)
-            usr = 'WHEN: '+when+'\nWHERE: '+where+'.\nDEBATE_EXCERPT:\n'
-            usr+= '\n'.join(debate[frm:to-1])
-            name=row['speaker']
-            sys='Summarize claim in the last sentence of this debate excerpt made by '+name+'.dont mention sentence was last. The summary should be factually self-contained containin all minimum facts for factchecking later begin alwais with who claims to who where(event name +city + state if available) when(date with month in word form)  . Start the summary by specifying the topic under discussion and how '+name+' is claimed in this line, including details of '+name+'s counterarguments or supporting points if made before and needed for this line. make sure selfcontained sentence always contains where and when statement was made to who and why.'
+            to = int(row['line_number'])
+            frm = max(0, (to - 1) - 10)
+            usr = 'WHEN: ' + when + '\nWHERE: ' + where + '.\nDEBATE_EXCERPT:\n' + '\n'.join(debate[frm:to - 1])
+            name = row['speaker']
+
+            # Defining the prompt for OpenAI's model
+            sys = 'Summarize claim in the last sentence of this debate excerpt made by ' + name + '. [Instructions for summary generation]'
+
+            # Generating summary using OpenAI's Chat Completion model
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 temperature=0,
@@ -46,6 +64,6 @@ for file in os.listdir(csv_dir):
                     {"role": "user", "content": usr}
                 ]
             )
-            output= response.choices[0].message['content']   
+            output = response.choices[0].message['content']
             of.at[index, 'summary'] = output 
-            of.to_csv('summaries/'+file, index=False)
+            of.to_csv('summaries/' + file, index=False)
